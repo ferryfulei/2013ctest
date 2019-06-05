f65e6519bfe24c91439cfc8deedc68732903f47f
Lei Fu <leifu@Leis-MacBook-Pro.local>
2019-06-05 01:12:18 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/eliza_skeleton.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/leifu/Desktop/eliza_skeleton/eliza.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/eliza.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/eliza_state.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/eliza_state.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/list.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/list.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/map.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/map.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/parser.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/parser.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/rule.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/rule.c.o"
+  "/Users/leifu/Desktop/eliza_skeleton/string_utils.c" "/Users/leifu/Desktop/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/string_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/eliza_skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eliza_skeleton.dir/eliza.c.o"
+  "CMakeFiles/eliza_skeleton.dir/eliza_state.c.o"
+  "CMakeFiles/eliza_skeleton.dir/list.c.o"
+  "CMakeFiles/eliza_skeleton.dir/map.c.o"
+  "CMakeFiles/eliza_skeleton.dir/parser.c.o"
+  "CMakeFiles/eliza_skeleton.dir/rule.c.o"
+  "CMakeFiles/eliza_skeleton.dir/string_utils.c.o"
+  "eliza_skeleton.pdb"
+  "eliza_skeleton"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eliza_skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
